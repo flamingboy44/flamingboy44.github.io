872305fda09d2c85d0b2f59ab63045e3b58f1015
flamingboy44 <93177178+flamingboy44@users.noreply.github.com>
2024-07-30 03:08:15 -0400

    Create Riding Round With My Soldiers!.lua

---
+++ Riding Round With My Soldiers!.lua
@@ -0,0 +1,26 @@
+getgenv().itemtoo = {
+            Gojo_Unleashed = {
+                [1] = "Infinity Orb",
+                [2] = "Gojo [Unleashed] [LV.7500]"
+            };
+            Altoria_Alter = {
+                [1] = "Corrupted Grail",
+                [2] = "Artoria Alter [LV. 8500]"
+            };
+            SukunaHalf = {
+                [1] = "Sukuna Fingers Bag",
+                [2] = "Sukuna [Half Power] [LV.7500]"
+            };
+            Choso = {
+                [1] = "[Choso] Cursed Womb",
+                [2] = "Choso [LV.8500]"
+            };
+            Meguna = {
+                [1] = "Infinity Orb Bag",
+                [2] = "Sukuna [Megumi] [LV. 8500]"
+            };
+            Killua = {
+                [1] = "Heart Bag",
+                [2] = "Killua [LV.6250]"
+            };
+        }
